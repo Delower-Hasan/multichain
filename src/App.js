35ee3ea60cc50f4da7206c779c524150299a6b2f
delower-hasan <dhhasansaha11@gmail.com>
2022-06-06 00:12:49 +0600

    metamask connection

--- src/App.js
+++ src/App.js
@@ -2,36 +2,96 @@ import "./App.css";
 import Header from './components/Header';
 
 import { FaSpaceShuttle } from "react-icons/fa";
+// import Web3 from 'web3'
+import { useEffect, useState } from "react";
+import {ethers} from "ethers";
 
 function App() {
+  const [errorMessage, setErrorMessage] = useState(null);
+	const [defaultAccount, setDefaultAccount] = useState(null);
+	const [userBalance, setUserBalance] = useState(null);
+	const [connButtonText, setConnButtonText] = useState('Connect Wallet');
+
+	const connectWalletHandler = () => {
+		if (window.ethereum && window.ethereum.isMetaMask) {
+			console.log('MetaMask Here!');
+
+			window.ethereum.request({ method: 'eth_requestAccounts'})
+			.then(result => {
+				accountChangedHandler(result[0]);
+				setConnButtonText('Wallet Connected');
+				getAccountBalance(result[0]);
+			})
+			.catch(error => {
+				setErrorMessage(error.message);
+			
+			});
+
+		} else {
+			console.log('Need to install MetaMask');
+			setErrorMessage('Please install MetaMask browser extension to interact');
+		}
+	}
+
+	// update account, will cause component re-render
+	const accountChangedHandler = (newAccount) => {
+		setDefaultAccount(newAccount);
+		getAccountBalance(newAccount.toString());
+	}
+
+	const getAccountBalance = (account) => {
+		window.ethereum.request({method: 'eth_getBalance', params: [account, 'latest']})
+		.then(balance => {
+			setUserBalance(ethers.utils.formatEther(balance));
+		})
+		.catch(error => {
+			setErrorMessage(error.message);
+		});
+	};
+
+
+	const chainChangedHandler = () => {
+		// reload the page to avoid any errors with chain change mid use of application
+		window.location.reload();
+	}
+
+
+	// listen for account changes
+	window.ethereum.on('accountsChanged', accountChangedHandler);
+
+	window.ethereum.on('chainChanged', chainChangedHandler);
+
+  
   return (
     <>
     <header>
-        <Header/>
+        <Header connectWalletHandler={connectWalletHandler} account={defaultAccount} balance = {userBalance} />
     </header>
     <main>
     <div className="MultiChain-section bg-dark2">
     <h2 className="body-h2" > Want to launch your project on MultiChain? </h2>
+
     <a className="btn-apply" href="https://docs.google.com/forms/d/e/1FAIpQLSer-PUjhMDG5fmasXYkmvY3EgpTC-yaI4up5by6Hx5g_9wZaw/viewform" ><FaSpaceShuttle/>  Apply to Launch </a>
+
+
    </div>
 
    <div className="footer bg-dark2"> 
       <div className="container">
             <div className="rosadfw d-flex align-items-center justify-content-between">
-                <p> Participants/Citizens from the following countries are strictly excluded/not allowed to participate in the IDOs: Bolivia, Cambodia, Iran, Iraq, Libya, Nepal, Zimbabwe, Liberia, Myanmar, North Korea.</p>
-                <p>	&copy; Copyright MultiChain 2022. All rights reserved.</p>
+                <p> Participants/Citizens from the following countries are strictly excluded/not allowed to participate in the IDOs: Bolivia, Cambodia, Iran, Iraq, Libya, Nepal, Zimbabwe, Liberia, Myanmar, North Korea .</p>
+                <p >	&copy; Copyright MultiChain 2022. All rights reserved.</p>
             </div>    
         </div>
    </div>
-    
-   
- 
+
+      
+
+
     </main>
 
-
-   
     </>
-  );
+  )
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,46 @@
-import React from 'react'
+
+import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
 import { Container, Nav, Navbar } from 'react-bootstrap'
+import React,{ useEffect, useState } from 'react'
 import headerImg from '../images/header.png'
 import logo from '../images/logo.svg'
 import pancake from "../images/pancake.svg"
-export default function header() {
+import metamask from '../images/metamask.svg'
+import ConnectWallet from "../images/walletConnect.svg"
+
+import { FaRegCopy } from "react-icons/fa";
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    borderRadius:'10px'
+  },
+};
+Modal.setAppElement("body");
+export default function Header({connectWalletHandler, account, balance}) {
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  useEffect(()=>{
+    account ? setIsOpen(false) : setIsOpen(true)
+    
+  },[account])
+
   return (
     <>
     <div className='header' style={{background:`url(${headerImg})`}}>
@@ -25,13 +62,58 @@ export default function header() {
       <Nav className="ms-auto align-items-center">
         <Nav.Link href="#home">Airdrop  🔥</Nav.Link>
         <Nav.Link href="#link">FAQ</Nav.Link>
-        <Nav.Link href="#link" title="Buy on PancakeSwap" className="btn-main"> <img src={pancake} className="pancake"/>Buy TPAD</Nav.Link>
-        <Nav.Link href="#link" className="btn-main"> Connect Wallet </Nav.Link>
+        <Nav.Link href="https://pancakeswap.finance/swap#/swap?inputCurrency=0xADCFC6bf853a0a8ad7f9Ff4244140D10cf01363C" title="Buy on PancakeSwap" className="btn-main"> <img src={pancake} className="pancake"/>Buy TPAD</Nav.Link>
+
+        {!account &&  <Nav.Link href="#link" className="btn-main"  onClick={openModal} > Connect Wallet </Nav.Link>}
+
+         {account && 
+          <Nav.Link href="#link" className="" > 
+            <div className='connectedWallet-wrap'>
+                <div className='walletEth'>
+                    <span className='eth'> {balance} ETH</span>
+                    <span className='pad'> 0 TPAD</span>
+                </div>
+                <div className='account-number'> 
+                <FaRegCopy/>{`${account?.slice(0,6)}...${account?.slice(-4)}`} 
+                
+                </div>
+                <svg className='userSvg' x="0" y="0" width="24" height="24"><rect x="0" y="0" width="24" height="24" transform="translate(1.3105926392727087 -1.0745306995694544) rotate(428.1 12 12)" fill="#03525E"></rect><rect x="0" y="0" width="24" height="24" transform="translate(8.457309516249648 5.217316426077632) rotate(153.7 12 12)" fill="#FB1864"></rect><rect x="0" y="0" width="24" height="24" transform="translate(-19.26527146003119 5.45189441438885) rotate(289.8 12 12)" fill="#FA7000"></rect></svg>
+            </div>
+
+        </Nav.Link>
+         }
+
       </Nav>
     </Navbar.Collapse>
   </Container>
 </Navbar>
     </div>
+
+
+    <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+      <div className='wallet-wrapper' >
+            <div className='wallet wallet-1' onClick={connectWalletHandler()} >
+                <img src={metamask}/>
+                <h2>MetaMask</h2>
+                <p>Connect to your MetaMask Wallet</p>
+            </div>
+            <hr/>
+            <div className='wallet wallet-2'>
+                <img src={ConnectWallet}/>
+                <h2>MetaMask</h2>
+                <p>Connect to your MetaMask Wallet</p>
+            </div>
+      </div>
+
+
+       
+       
+      </Modal>
 
     </>
   )
